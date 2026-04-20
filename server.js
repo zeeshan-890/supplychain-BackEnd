e2a1060439671491d56043d38bd3427d6a282904
Muhammad Zeeshan Abbas <zzabbaskhan830@gmail.com>
2026-04-20 17:51:00 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 import app from "./app.js";
 import dotenv from "dotenv";
-import { createAdminUser } from "./scripts/createAdminUser.js";
-import addDistributors from "./scripts/addDistributors.js";
+
 dotenv.config();
 
 //Start Server
 app.listen(process.env.PORT || 3000, async () => {
   console.log(`Server started at port ${process.env.PORT || 3000}...`);
 
-  // Create admin user if it doesn't exist
-  await createAdminUser();
-
-  // Initialize default distributors if they don't exist
-  await addDistributors();
+  
 });
