6fef6625df86b229ce44b722cdffa9a5b8c931a2
Muhammad Zeeshan Abbas <zzabbaskhan830@gmail.com>
2026-04-20 17:47:38 +0500

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(helmet({
 //CORS --> Restrict to allowed origins in production
 const allowedOrigins = process.env.ALLOWED_ORIGINS
   ? process.env.ALLOWED_ORIGINS.split(",")
-  : ["http://localhost:3000", "http://localhost:5173"];
+  : ["http://localhost:3000", "http://localhost:5173","https://supplychain-client.vercel.app"];
 
 console.log("Allowed CORS origins:", allowedOrigins);
 
